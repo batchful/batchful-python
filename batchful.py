11fb042191899a2cc9148c3cca61410bf25e089e
3174N <63750650+3174N@users.noreply.github.com>
2020-06-03 19:55:09 +0300

    Added GitHub Link

--- batchful.py
+++ batchful.py
@@ -1,9 +1,42 @@
+import webbrowser
+
 batchfulLogo = " _             _         _       __         _ \n| |__    __ _ | |_  ___ | |__   / _| _   _ | |\n| '_ \  / _` || __|/ __|| '_ \ | |_ | | | || |\n| |_) || (_| || |_| (__ | | | ||  _|| |_| || |\n|_.__/  \__,_| \__|\___||_| |_||_|   \__,_||_|"
 
-def ByExt:
+def ByExt():
     return
 
-def ByName:
+def ByName():
     return
+
+def GitHub():
+    print ("Made by 3174N and SFR-git \n")
+    print ("Open GitHub? [Y/n] \n")
+
+    answer = input("")
+
+    if answer == "Y" or answer == "y":
+        webbrowser.open("https://github.com/batchful/batchful-python")
+
+def Help():
+    return
+
+def Ask():
+    print ("This program organizes folders. place this file in the directory you wish to organize and run it.")
+    print ("Choose a method of organization: \n")
+    print ("1. By file extensions.")
+    print ("2. By file names. \n \n")
+    print ("Press g for the GitHub repository page, h for help and q to exit the program. \n")
+
+    answer = input("")
+
+    if answer == "1":
+        ByExt()
+    elif answer == "2":
+        ByName()
+    elif answer == "G" or answer == "g":
+        GitHub()
+    elif answer == "H" or answer == "h":
+        Help()
 
 print (batchfulLogo)
+Ask()
